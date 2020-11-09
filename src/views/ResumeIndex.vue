5eb28eb9878f49dfc423c420f99ca7e99979b641
ng315429 <ng315429@gmail.com>
2020-11-09 20:21:52 +0900

    fix

--- src/views/ResumeIndex.vue
+++ src/views/ResumeIndex.vue
@@ -35,13 +35,9 @@
           3년차 프론트 개발자로 모빌리티, 커머스 등의 스타트업에서 웹과 앱 서비스를 개발/배포/운영 경험이 있습니다.<br />
           주로 웹 기반 개발을 담당했으며 팀내 필요에 따라 백엔드 개발 역할도 겸하였습니다.<br />
           <br />
-          작은 규모의 팀에 초기 개발자로 입사하여 프론트와 백엔드 영억을 개발하면서 <br />
+          작은 규모의 팀에 초기 개발자로 입사하여 프론트와 백엔드 영역을 개발하면서 <br />
           동일 서비스 중 높은 인지도까지의 성장, 그로인한 다양한 기업들과의 제휴 경험이 있습니다. <br />
           혼자가 아닌 함께 이뤄낼 수 있다고 생각하기에 다양한 구성원들과 적극적으로 협업해왔습니다. <br />
-          <br />
-          깔끔한 인터페이스, 애니메이션 구현하는 것을 즐겨합니다.<br />
-          <br />
-          주도적으로 개발 업무를 진행할 수 있는 환경을 선호합니다.
         </p>
       </template>
     </base-content>
@@ -85,7 +81,6 @@
           <div class="skill__desc">
             <ul>
               <li>AWS(RDS, S3, CloudFont, Cloudwatch, Route53, EC2)</li>
-              <li>Docker, Travis CI</li>
             </ul>
           </div>
         </div>
@@ -106,18 +101,18 @@
 
           <div class="experience-box">
             <div class="experience__desc">
-              <h4 class="title">결제 이전 및 개편</h4>
+              <h4 class="title">결제 API 개발</h4>
               <p class="period">2020.03 ~ 2020.05</p>
               <div class="description">
-                <p class="summary">결제 API 개발 및 PG사 변경</p>
+                <p class="summary">결제 API 이전 및 PG사 변경으로 인한 신규 개발</p>
                 <ul>
-                  <li>운영, 어드민, 주문중개로 분리된 API 통합 및 결제내 기능별 모듈화</li>
-                  <li>쿠폰, 포인트, 상품 등 검증 로직 개선</li>
+                  <li>웹/앱 통신으로 결제 처리</li>
+                  <li>결제 API 신규 개발</li>
+                  <li>운영, 어드민, 주문중개로 분리된 API 통합</li>
                   <li>결제전/후 주문, 상품 등 검증 실패에 따른 결제취소 로직 구현</li>
-                  <li>상품 부분취소 로직 구현</li>
-                  <li>결제 데이터 구조 설계</li>
+                  <li>결제내 상품 부분취소 추가</li>
                 </ul>
-                <p class="tech">JavaScript, Python(Django), PHP</p>
+                <p class="tech">JavaScript, Python(Django)</p>
               </div>
             </div>
 
@@ -125,11 +120,12 @@
               <h4 class="title">휴대폰본인인증/성인인증 API 개발</h4>
               <p class="period">2020.03 ~ 2020.03</p>
               <div class="description">
-                <p class="summary">성인인증이 필요한 상품들 유입으로 인해 성인인증 시스템 도입</p>
+                <p class="summary">성인인증 상품 추가</p>
                 <ul>
-                  <li>상품목록내 성인인증 로직 및 UI 개발</li>
-                  <li>휴대폰 본인인증(KG모빌리언스) API 연동</li>
-                  <li>연동 후 인증 처리 API 개발</li>
+                  <li>웹/앱 통신으로 인증 처리</li>
+                  <li>일반상품/성인상품 UI/기능 구분 처리</li>
+                  <li>본인인증 API 개발</li>
+                  <!-- <li>휴대폰 본인인증(KG모빌리언스) API 연동</li> -->
                 </ul>
                 <p class="tech">JavaScript, Python(Django), HTML, CSS</p>
               </div>
@@ -139,18 +135,20 @@
               <h4 class="title">회원통합 및 로그인/회원가입 개편</h4>
               <p class="period">2020.02 ~ 2020.04</p>
               <div class="description">
-                <p class="summary">일반계정, 소셜계정 통합 및 로그인/회원가입 절차 개편</p>
+                <p class="summary">일반계정, 소셜계정 통합 및 로그인/회원가입 리뉴얼</p>
                 <ul>
-                  <li>휴대전화번호 기준으로 회원통합/연동</li>
+                  <li>웹/앱 통신으로 로그인/가입 처리</li>
+                  <li>회원통합 상태에 따른 UI/기능 구분 처리</li>
                   <li>소셜 가입시 회원여부, 인증 및 가입 절차 API 개발</li>
                   <li>소셜 로그인시 일반계정 연동 API 개발</li>
-                  <li>1차 PHP로 리뉴얼, 2차 Python(Django)로 리뉴얼</li>
+                  <!-- <li>휴대전화번호 기준으로 회원통합/연동</li> -->
+                  <!-- <li>1차 PHP로 리뉴얼, 2차 Python(Django)로 리뉴얼</li> -->
                 </ul>
-                <p class="tech">PHP, JavaScript, Python(Django)</p>
+                <p class="tech">JavaScript, Python(Django), PHP</p>
               </div>
             </div>
 
-            <div class="experience__desc">
+            <!-- <div class="experience__desc">
               <h4 class="title">편의점(세븐일레븐) 제휴 서비스 런칭</h4>
               <p class="period">2020.01 ~ 2020.02</p>
               <div class="description">
@@ -162,11 +160,11 @@
                 </ul>
                 <p class="tech">PHP, JavaScript, HTML, CSS, Onsen Framework</p>
               </div>
-            </div>
+            </div> -->
 
             <div class="experience__desc">
               <h4 class="title">지도 서비스 개편</h4>
-              <p class="period">2020.01 ~ 2020.01</p>
+              <p class="period">2020.01 ~ 2020.02</p>
               <div class="description">
                 <p class="summary">카카오 지도 API를 사용하여 지도 커스텀 및 UI 리뉴얼</p>
                 <ul>
@@ -174,10 +172,10 @@
                     기존 모든 매장 위치를 마커로 인한 속도저하와 잦은 호출 문제를 영역범위, 드래그/줌 제어 등을 통해
                     속도 및 사용성 개선
                   </li>
-                  <li>매장, 제휴사, 판매가능여부, 동일좌표 등 여러 조건에 따라 UI 개발</li>
+                  <li>매장, 제휴사, 판매가능여부, 동일좌표 등 여러 조건에 따른 UI 개발</li>
                   <li>Redis를 사용하여 좌표 클러스터링 기능 개발</li>
                 </ul>
-                <p class="tech">JavaScript, Kakao Map, Python(Django), HTML, CSS, Onsen Framework</p>
+                <p class="tech">JavaScript, Kakao Map, Python(Django), HTML, CSS</p>
               </div>
             </div>
 
@@ -190,11 +188,12 @@
                   업주 전용앱
                 </p>
                 <ul>
-                  <li>프론트엔드/백엔드 기술환경 세팅 및 전반적인 화면 및 기능 개발</li>
-                  <li>컴포넌트 캐싱, 코드스플리팅을 통해 초기 로딩 속도 개선</li>
-                  <li>Firebase ML Kit (Barcode Scan)연동을 통해 빠른 상품등록 기능 및 화면 개발</li>
-                  <li>Socket통신을 통한 실시간 주문확인 구현</li>
-                  <li>접근 권한에 따른 화면 제어 및 로그인API 개선</li>
+                  <li>화면 및 기능 개발</li>
+                  <li>컴포넌트 캐싱, 코드스플리팅 등을 통한 속도 개선</li>
+                  <li>Firebase ML Kit (Barcode Scan)연동을 통해 빠른 바코드 등록 기능 및 화면 개발</li>
+                  <li>Socket통신을 통한 실시간 주문확인 기능 및 화면 개발</li>
+                  <li>Andriod, iOS 배포 및 관리</li>
+                  <li>백엔드 개발</li>
                 </ul>
                 <p class="tech">NativeScript-Vue, Vuex, Socket.io, Python(Django), Firebase, SCSS</p>
               </div>
@@ -205,11 +204,12 @@
               <p class="period">2019.08 ~ 2020.12</p>
               <div class="description">
                 <p class="summary">
-                  영업시 매장등록, 등록한 매장 제출, 매장관리 등을 제공하는 영업 전용앱
+                  영업자분들이 쉽게 매장을 등록할 수 있고 관리할 수 있는 영업 전용앱
                 </p>
                 <ul>
-                  <li>프론트엔드/백엔드 기술환경 세팅 및 전체 화면 및 기능 개발</li>
+                  <li>화면 및 기능 개발</li>
                   <li>트리형 사용자 권한 기능 구현</li>
+                  <li>백엔드 개발</li>
                 </ul>
                 <p class="tech">Vue, Vuex, JavaScript, Python(Django), Material-UI, HTML, SCSS</p>
               </div>
@@ -224,45 +224,29 @@
                   업주 전용앱
                 </p>
                 <ul>
-                  <li>프론트엔드/백엔드 기술환경 세팅 및 전반적인 화면 및 기능 개발</li>
-                  <li>컴포넌트 캐싱, 코드스플리팅을 통해 초기 로딩 속도 개선</li>
+                  <li>화면 및 기능 개발</li>
+                  <li>컴포넌트 캐싱, 코드스플리팅 등을 통한 속도 개선</li>
                   <li>업주 니즈에 맞게 화면 및 기능 개발/개선</li>
                   <li>Socket통신을 통한 실시간 주문확인 구현</li>
-                  <li>접근 권한에 따른 화면 제어 및 로그인API 개선</li>
+                  <li>백엔드 개발</li>
                   <li>추후 신규 업주용 앱 코드 베이스</li>
                 </ul>
                 <p class="tech">NativeScript-Vue, Vuex, Socket.io, Python(Django), SCSS</p>
               </div>
             </div>
 
-            <!-- <div class="experience__desc">
-              <h4 class="title">롯데카드 SSO 연동</h4>
-              <p class="period">2019.04 ~ 2019.05</p>
-              <div class="description">
-                <p class="summary">
-                  제휴사 롯데카드 회원과의 통합인증 기능 제공
-                </p>
-                <ul>
-                  <li>통합인증에 필요한 화면처리 구현</li>
-                  <li>주문, 결제 시 회원 구분 화면/기능 개발</li>
-                  <li>인증처리 API 개발</li>
-                </ul>
-                <p class="tech">JavaScript, PHP, Python(Django)</p>
-              </div>
-            </div> -->
-
             <div class="experience__desc">
               <h4 class="title">라스트오더 프론트/백엔드 개발 및 유지보수</h4>
               <p class="period">2019.02 ~ 2020.06</p>
               <div class="description">
-                <p class="summary"></p>
+                <p class="summary">운영 서비스</p>
                 <ul>
                   <li>기본적인 유지보수 및 운영 비롯해 비즈니스 요구에 맞는 다양한 화면/신기능 개발</li>
                   <!-- <li>PHP 템플릿(template_) 제거 작업</li> -->
                   <li>제휴사별 화면/기능 개발</li>
                   <li>리플로우, 리페인트 최소화로 렌더링 개선</li>
                   <li>스켈레톤 UI, 이미지 로딩 등을 통한 UX 개선</li>
-                  <li>훅을 화면에 맞게 구분하여 API 호출 최소화 및 사용성 개선</li>
+                  <li>적절한 훅 사용을 통한 API 호출 최소화 및 사용성 개선</li>
                   <li>마케팅, 이벤트에 필요한 리소스 통합 관리 화면/기능 개발</li>
                   <li>
                     Hash 및 history를 제어하여 브라우저 뒤로가기, 외부 공유 등의 상황에서 사용자가 바라보고 있던 화면이
@@ -272,11 +256,10 @@
                   <li>메인 화면 로드시 많은 API 호출을 통합, 최소화하여 속도 개선</li>
                   <li>배송, 포인트 정책 추가로 인한 화면 및 주문, 결제 로직 개발</li>
                   <li>기존 의존성 높은 데이터의 관계를 최소화하여 유지보수성 증가</li>
-                  <li>다양한 브라우저,앱 환경 대응</li>
+                  <li>다양한 브라우저, 앱 환경 대응</li>
                   <li>기존 PHP로 작성된 레거시를 Django로 부분 이전 작업</li>
                   <li>RDS read, master 구분 사용</li>
-                  <!-- <li>전반적인 Web, API, APP 배포 및 관리</li> -->
-                  <!-- <li>UI 리뉴얼</li> -->
+                  <li>프론트, 백엔드 배포 및 관리</li>
                 </ul>
                 <p class="tech">
                   JavaScript, jQuery, PHP, Python(Django), HTML, CSS, Webpack, MySQL, S3, Nginx, Apache, EC2, Docker,
@@ -303,7 +286,7 @@
               <div class="description">
                 <p class="summary">SK하이닉스 통근버스 앱 및 어드민 페이지 개발</p>
                 <ul>
-                  <li>사용자앱 웹뷰영역 화면 개발 및 API 개발</li>
+                  <li>화면 개발 및 API 개발</li>
                   <li>어드민 페이지 화면 개발 및 API 개발</li>
                   <li>위치, 시간에 따른 동적 UI 구현</li>
                   <li>초기 사용자앱 화면 기획</li>
@@ -314,7 +297,7 @@
           </div>
         </div>
 
-        <!-- <div class="experience">
+        <div class="experience">
           <div class="experience__title">
             <h3 class="title">디오네르(크라운구스)</h3>
             <div class="description">
@@ -339,7 +322,7 @@
               </div>
             </div>
           </div>
-        </div> -->
+        </div>
       </template>
     </base-content>
 
@@ -364,7 +347,7 @@
                   <li>전체 화면 개발</li>
                   <li>많은 동적 필터(API CALL)를 Promise All 처리 및 모듈화로 성능 개선</li>
                   <li>Firebase Auth 연동으로 기능 권한 처리</li>
-                  <li>AWS S3 Deploy 환경 구축</li>
+                  <li>AWS Deploy 환경 구축</li>
                 </ul>
                 <p class="tech">Vue.js, Element UI, JavaScript, Firebase Auth</p>
               </div>
