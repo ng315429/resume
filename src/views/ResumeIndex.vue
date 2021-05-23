b3417b9ebaee96c6d80d280556aea27a65651591
ng315429 <ng315429@gmail.com>
2021-05-23 16:09:27 +0900

    feat: 사항 추가

--- src/views/ResumeIndex.vue
+++ src/views/ResumeIndex.vue
@@ -32,12 +32,15 @@
       <template v-slot:content-title>Introduce.</template>
       <template v-slot:content-desc>
         <p>
-          4년차 프론트 개발자로 모빌리티, 커머스 등의 스타트업에서 웹과 앱 서비스를 개발/배포/운영 경험이 있습니다.<br />
+          4년차 프론트 개발자로 모빌리티, 커머스 등의 스타트업에서 웹과 앱 서비스를 개발/배포/운영
+          경험이 있습니다.<br />
           주로 웹 기반 개발을 담당했으며 팀내 필요에 따라 백엔드 개발 역할도 겸하였습니다.<br />
           <br />
           작은 규모의 팀에 초기 개발자로 입사하여 프론트와 백엔드 영역을 개발하면서 <br />
-          동일 서비스 중 높은 인지도까지의 성장, 그로인한 다양한 기업들과의 제휴 경험이 있습니다. <br />
-          혼자가 아닌 함께 이뤄낼 수 있다고 생각하기에 다양한 구성원들과 적극적으로 협업해왔습니다. <br />
+          동일 서비스 중 높은 인지도까지의 성장, 그로인한 다양한 기업들과의 제휴 경험이 있습니다.
+          <br />
+          혼자가 아닌 함께 이뤄낼 수 있다고 생각하기에 다양한 구성원들과 적극적으로 협업해왔습니다.
+          <br />
         </p>
       </template>
     </base-content>
@@ -92,6 +95,16 @@
       <template slot="content-desc">
         <div class="experience">
           <div class="experience__title">
+            <h3 class="title">에이블리코퍼레이션(에이블리)</h3>
+            <div class="description">
+              <p class="job">Front-End</p>
+              <p class="period">2021.01 ~</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="experience">
+          <div class="experience__title">
             <h3 class="title">미로(라스트오더)</h3>
             <div class="description">
               <p class="job">Front-End, Back-End</p>
@@ -104,12 +117,16 @@
               <h4 class="title">결제 API 개발</h4>
               <p class="period">2020.03 ~ 2020.05</p>
               <div class="description">
-                <p class="summary">결제 API 이전 및 PG사 변경으로 인한 신규 개발</p>
+                <p class="summary">
+                  결제 API 이전 및 PG사 변경으로 인한 신규 개발
+                </p>
                 <ul>
                   <li>웹/앱 통신으로 결제 처리</li>
                   <li>결제 API 신규 개발</li>
                   <li>운영, 어드민, 주문중개로 분리된 API 통합</li>
-                  <li>결제전/후 주문, 상품 등 검증 실패에 따른 결제취소 로직 구현</li>
+                  <li>
+                    결제전/후 주문, 상품 등 검증 실패에 따른 결제취소 로직 구현
+                  </li>
                   <li>결제내 상품 부분취소 추가</li>
                 </ul>
                 <p class="tech">JavaScript, Python(Django)</p>
@@ -135,7 +152,9 @@
               <h4 class="title">회원통합 및 로그인/회원가입 개편</h4>
               <p class="period">2020.02 ~ 2020.04</p>
               <div class="description">
-                <p class="summary">일반계정, 소셜계정 통합 및 로그인/회원가입 리뉴얼</p>
+                <p class="summary">
+                  일반계정, 소셜계정 통합 및 로그인/회원가입 리뉴얼
+                </p>
                 <ul>
                   <li>웹/앱 통신으로 로그인/가입 처리</li>
                   <li>회원통합 상태에 따른 UI/기능 구분 처리</li>
@@ -166,17 +185,23 @@
               <h4 class="title">지도 서비스 개편</h4>
               <p class="period">2020.01 ~ 2020.02</p>
               <div class="description">
-                <p class="summary">카카오 지도 API를 사용하여 지도 커스텀 및 UI 리뉴얼</p>
+                <p class="summary">
+                  카카오 지도 API를 사용하여 지도 커스텀 및 UI 리뉴얼
+                </p>
                 <ul>
                   <li>
-                    기존 모든 매장 위치를 마커로 인한 속도저하와 잦은 호출 문제를 영역범위, 드래그/줌 제어 등을 통해
-                    속도 및 사용성 개선
+                    기존 모든 매장 위치를 마커로 인한 속도저하와 잦은 호출 문제를 영역범위,
+                    드래그/줌 제어 등을 통해 속도 및 사용성 개선
                   </li>
-                  <li>매장, 제휴사, 판매가능여부, 동일좌표 등 여러 조건에 따른 UI 개발</li>
+                  <li>
+                    매장, 제휴사, 판매가능여부, 동일좌표 등 여러 조건에 따른 UI 개발
+                  </li>
                   <li>Redis를 사용하여 좌표 클러스터링 기능 개발</li>
                   <li>위치기반 API 개발</li>
                 </ul>
-                <p class="tech">JavaScript, Kakao Map, Python(Django), HTML, CSS</p>
+                <p class="tech">
+                  JavaScript, Kakao Map, Python(Django), HTML, CSS
+                </p>
               </div>
             </div>
 
@@ -185,18 +210,22 @@
               <p class="period">2019.12 ~ 2020.05</p>
               <div class="description">
                 <p class="summary">
-                  주문관리(주문확정, 주문완료, 주문취소), 실시간 주문확인, 상품등관리, 통계 등의 기능을 제공하는 편의점
-                  업주 전용앱
+                  주문관리(주문확정, 주문완료, 주문취소), 실시간 주문확인, 상품등관리, 통계 등의
+                  기능을 제공하는 편의점 업주 전용앱
                 </p>
                 <ul>
                   <li>화면 및 기능 개발</li>
                   <li>컴포넌트 캐싱, 코드스플리팅 등을 통한 속도 개선</li>
-                  <li>Firebase ML Kit (Barcode Scan)연동을 통해 빠른 바코드 등록 기능 및 화면 개발</li>
+                  <li>
+                    Firebase ML Kit (Barcode Scan)연동을 통해 빠른 바코드 등록 기능 및 화면 개발
+                  </li>
                   <li>Socket통신을 통한 실시간 주문확인 기능 및 화면 개발</li>
                   <li>Andriod, iOS 배포 및 관리</li>
                   <li>백엔드 개발</li>
                 </ul>
-                <p class="tech">NativeScript-Vue, Vuex, Socket.io, Python(Django), Firebase, SCSS</p>
+                <p class="tech">
+                  NativeScript-Vue, Vuex, Socket.io, Python(Django), Firebase, SCSS
+                </p>
               </div>
             </div>
 
@@ -212,7 +241,9 @@
                   <li>트리형 사용자 권한 기능 구현</li>
                   <li>백엔드 개발</li>
                 </ul>
-                <p class="tech">Vue, Vuex, JavaScript, Python(Django), Material-UI, HTML, SCSS</p>
+                <p class="tech">
+                  Vue, Vuex, JavaScript, Python(Django), Material-UI, HTML, SCSS
+                </p>
               </div>
             </div>
 
@@ -221,8 +252,8 @@
               <p class="period">2019.06 ~ 2020.04</p>
               <div class="description">
                 <p class="summary">
-                  주문관리(주문확정, 주문완료, 주문취소), 실시간 주문확인, 상품등관리, 통계 등의 기능을 제공하는 일반
-                  업주 전용앱
+                  주문관리(주문확정, 주문완료, 주문취소), 실시간 주문확인, 상품등관리, 통계 등의
+                  기능을 제공하는 일반 업주 전용앱
                 </p>
                 <ul>
                   <li>화면 및 기능 개발</li>
@@ -232,7 +263,9 @@
                   <li>백엔드 개발</li>
                   <li>추후 신규 업주용 앱 코드 베이스</li>
                 </ul>
-                <p class="tech">NativeScript-Vue, Vuex, Socket.io, Python(Django), SCSS</p>
+                <p class="tech">
+                  NativeScript-Vue, Vuex, Socket.io, Python(Django), SCSS
+                </p>
               </div>
             </div>
 
@@ -242,29 +275,41 @@
               <div class="description">
                 <p class="summary">운영 서비스</p>
                 <ul>
-                  <li>기본적인 유지보수 및 운영 비롯해 비즈니스 요구에 맞는 다양한 화면/신기능 개발</li>
+                  <li>
+                    기본적인 유지보수 및 운영 비롯해 비즈니스 요구에 맞는 다양한 화면/신기능 개발
+                  </li>
                   <!-- <li>PHP 템플릿(template_) 제거 작업</li> -->
                   <li>제휴사별 화면/기능 개발</li>
                   <li>리플로우, 리페인트 최소화로 렌더링 개선</li>
                   <li>스켈레톤 UI, 이미지 로딩 등을 통한 UX 개선</li>
                   <li>적절한 훅 사용을 통한 API 호출 최소화 및 사용성 개선</li>
-                  <li>마케팅, 이벤트에 필요한 리소스 통합 관리 화면/기능 개발</li>
                   <li>
-                    Hash 및 history를 제어하여 브라우저 뒤로가기, 외부 공유 등의 상황에서 사용자가 바라보고 있던 화면이
-                    유지되도록 개발
+                    마케팅, 이벤트에 필요한 리소스 통합 관리 화면/기능 개발
                   </li>
-                  <li>URL Query String를 사용하여 라우터 기능 개발 및 마케팅 목적의 페이지 개발</li>
-                  <li>메인 화면 로드시 많은 API 호출을 통합, 최소화하여 속도 개선</li>
-                  <li>배송, 포인트 정책 추가로 인한 화면 및 주문, 결제 로직 개발</li>
-                  <li>기존 의존성 높은 데이터의 관계를 최소화하여 유지보수성 증가</li>
+                  <li>
+                    Hash 및 history를 제어하여 브라우저 뒤로가기, 외부 공유 등의 상황에서 사용자가
+                    바라보고 있던 화면이 유지되도록 개발
+                  </li>
+                  <li>
+                    URL Query String를 사용하여 라우터 기능 개발 및 마케팅 목적의 페이지 개발
+                  </li>
+                  <li>
+                    메인 화면 로드시 많은 API 호출을 통합, 최소화하여 속도 개선
+                  </li>
+                  <li>
+                    배송, 포인트 정책 추가로 인한 화면 및 주문, 결제 로직 개발
+                  </li>
+                  <li>
+                    기존 의존성 높은 데이터의 관계를 최소화하여 유지보수성 증가
+                  </li>
                   <li>다양한 브라우저, 앱 환경 대응</li>
                   <li>기존 PHP로 작성된 레거시를 Django로 부분 이전 작업</li>
                   <li>RDS read, master 구분 사용</li>
                   <li>프론트, 백엔드 배포 및 관리</li>
                 </ul>
                 <p class="tech">
-                  JavaScript, jQuery, PHP, Python(Django), HTML, CSS, Webpack, MySQL, S3, Nginx, Apache, EC2, Docker,
-                  Travis CI, Onsen UI
+                  JavaScript, jQuery, PHP, Python(Django), HTML, CSS, Webpack, MySQL, S3, Nginx,
+                  Apache, EC2, Docker, Travis CI, Onsen UI
                 </p>
               </div>
             </div>
@@ -285,14 +330,18 @@
               <h4 class="title">SK하이닉스 통근버스 관제시스템</h4>
               <p class="period">2018.04 ~ 2018.10</p>
               <div class="description">
-                <p class="summary">SK하이닉스 통근버스 앱 및 어드민 페이지 개발</p>
+                <p class="summary">
+                  SK하이닉스 통근버스 앱 및 어드민 페이지 개발
+                </p>
                 <ul>
                   <li>화면 개발 및 API 개발</li>
                   <li>어드민 페이지 화면 개발 및 API 개발</li>
                   <li>위치, 시간에 따른 동적 UI 구현</li>
                   <li>초기 사용자앱 화면 기획</li>
                 </ul>
-                <p class="tech">JavaScript, HTML, CSS, jQuery, Java, Spring Boot, MyBatis, MySQL</p>
+                <p class="tech">
+                  JavaScript, HTML, CSS, jQuery, Java, Spring Boot, MyBatis, MySQL
+                </p>
               </div>
             </div>
           </div>
@@ -350,7 +399,9 @@
                   <li>Firebase Auth 연동으로 기능 권한 처리</li>
                   <li>AWS Deploy 환경 구축</li>
                 </ul>
-                <p class="tech">Vue.js, Element UI, JavaScript, Firebase Auth</p>
+                <p class="tech">
+                  Vue.js, Element UI, JavaScript, Firebase Auth
+                </p>
               </div>
             </div>
           </div>
